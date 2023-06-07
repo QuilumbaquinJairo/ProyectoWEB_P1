19489c3eb2b1d7ac66d315d884ccaa1029dff9ae
Jairo Quilumbaquin <jairoquilumbaquin1@gmail.com>
2023-06-06 22:35:11 -0500

    updated

--- php/envio.php
+++ php/envio.php
@@ -1,17 +1,39 @@
 <?php
-require 'path/to/PHPMailer/PHPMailerAutoload.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-if (isset($_POST['submit'])) {
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $telefono = $_POST['telefono'];
     $provincia = $_POST['province'];
     $comentario = $_POST['textarea'];
+    $mail = new PHPMailer(true);
 
-    // Send email
-    $mail = new PHPMailer;
-    $mail->setFrom($email);
-    $mail->addAddress('jsquilumbaquin@espe.edu.ec');
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('noreply@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Form Submission';
     $mail->Body = "Nombre: " . $nombre . "\n"
         . "Email: " . $email . "\n"
@@ -19,11 +41,10 @@ if (isset($_POST['submit'])) {
         . "Provincia: " . $provincia . "\n"
         . "Comentario: " . $comentario;
 
-    if ($mail->send()) {
-        echo 'Email sent successfully';
-    } else {
-        echo 'Error sending email: ' . $mail->ErrorInfo;
-    }
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
 
+?>
